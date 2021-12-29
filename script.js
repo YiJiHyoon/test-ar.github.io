f0da7b95dafd225450740cd2448720694bdfdbb1
YiJiHyoon <ooz96@naver.com>
2021-12-29 11:38:05 +0900

    .

--- script.js
+++ script.js
@@ -22,12 +22,13 @@ function renderPlaces(places) {
         let latitude = place.location.lat;
         let longitude = place.location.lng;
 
-        let model = document.createElement('a-entity');
+        let model = document.createElement('a-text');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-        model.setAttribute('rotation', '0 180 0');
-        model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.5 0.5 0.5');
+        model.setAttribute('value', `메가박스`);
+        // model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+        // model.setAttribute('rotation', '0 180 0');
+        // model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '120 120 120');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
