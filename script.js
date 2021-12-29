c0e97a5da082ec2172a5256e42abb271381e9672
YiJiHyoon <ooz96@naver.com>
2021-12-29 11:48:08 +0900

    .

--- script.js
+++ script.js
@@ -22,13 +22,13 @@ function renderPlaces(places) {
         let latitude = place.location.lat;
         let longitude = place.location.lng;
 
-        let model = document.createElement('a-text');
+        let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         model.setAttribute('value', `메가박스`);
-        // model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
         // model.setAttribute('rotation', '0 180 0');
         // model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '120 120 120');
+        model.setAttribute('scale', '0.5 0.5 0.5');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
